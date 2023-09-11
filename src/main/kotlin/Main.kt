56c34325f382d2960a0b3ca08d25ce86974a2442
Dschids <123191756+Dschids@users.noreply.github.com>
2023-09-11 16:39:40 -0500

    now with twice as many functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -13,6 +13,25 @@ fun customerinfo(busname: String, name: String, number: String){
     println(name)
     println(number)
 }
+// residential function gets the name and number of customer and passes it to customerInfo()
+fun residential(){
+    println("Enter your name: ")
+    var name = readln()!!.toString()
+    println("Enter your phone number: ")
+    var number = readln()!!.toString()
+    customerinfo(name, number)
+}
+// business function gets the business name, contact name and contact number of customer and passes it
+// to customerInfo()
+fun business(){
+    println("Enter your business name: ")
+    var busname = readln()!!.toString()
+    println("Enter the contact name: ")
+    var name = readln()!!.toString()
+    println("Enter the contact phone number: ")
+    var number = readln()!!.toString()
+    customerinfo(busname, name, number)
+}
 // main function asks the user if they are a business or residential customer, residential then aks
 // for 2 arguments name and number and passes them to customerinfo(), business asks for 3 arguments
 // business name, contact name, and contact phone number then passes them to customerinfo() if the
@@ -21,22 +40,12 @@ fun main(args: Array<String>) {
     println("Are you a business or residential customer?")
     var cust = readln()!!.toString()
     if (cust.toLowerCase() == "residential") {
-        println("Enter your name: ")
-        var name = readln()!!.toString()
-        println("Enter your phone number: ")
-        var number = readln()!!.toString()
-        customerinfo(name, number)
+        residential()
     }
     else if (cust.toLowerCase() == "business"){
-        println("Enter your business name: ")
-        var busname = readln()!!.toString()
-        println("Enter the contact name: ")
-        var name = readln()!!.toString()
-        println("Enter the contact phone number: ")
-        var number = readln()!!.toString()
-        customerinfo(busname, name, number)
+        business()
     }
     else{
-        println("Selection invalid")
+        println("Customer type invalid")
     }
 }
